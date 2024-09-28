0b6d44084244cace3ff0a97bd7172a80246fa33b
gayathris99 <gayathri11499@gmail.com>
2024-09-28 20:24:00 +0530

    Route Setup

--- src/components/index.js
+++ src/components/index.js
@@ -4,6 +4,7 @@ import Navbar from './Navbar';
 import About from './About';
 import Tech from './Tech';
 import Experience from './Experience';
+import Loader from './Loader';
 import Works from './Works';
 import Feedbacks from './Feedbacks';
 import Contact from './Contact';
@@ -14,6 +15,7 @@ export {
   About,
   Tech,
   Experience,
+  Loader,
   Works,
   Feedbacks,
   Contact,
